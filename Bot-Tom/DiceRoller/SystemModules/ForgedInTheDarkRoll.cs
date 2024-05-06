0f5439c7388993abed3552eea7789a3a2aaa63a5
SEE YOU SPACE BUNNY <see.you.space.bunny@protonmail.com>
2024-05-06 23:27:56 +0200

    Added empty classes for HWI and FITD

---
+++ Bot-Tom/DiceRoller/SystemModules/ForgedInTheDarkRoll.cs
@@ -0,0 +1,23 @@
+
+namespace BotTom;
+
+/// <summary>
+/// Rolling dice for Forged In The Dark game systems.
+/// </summary>
+/// <param name="dicePool"></param>
+/// <param name="label"></param>
+class ForgedInTheDarkRoll(long dicePool, string? label)
+{
+  long DicePool { get; } = dicePool;
+  string? Label { get; } = label;
+
+
+  public override string ToString()
+  {
+      StringBuilder sb = new();
+      if(Label is not null)
+          sb.AppendLine($"> {Label.Replace("\\n","\n> ")}");
+          
+      return sb.ToString();
+  }
+}
