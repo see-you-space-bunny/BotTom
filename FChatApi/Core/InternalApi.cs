ffbda9ff5d259f9fe1b6b5fe6aeb4841d8265f8a
SEE YOU SPACE BUNNY <see.you.space.bunny@protonmail.com>
2024-07-08 01:31:55 +0200

    indentation & renamed hycybh local

--- FChatApi/Core/InternalApi.cs
+++ FChatApi/Core/InternalApi.cs
@@ -95,17 +95,17 @@ public partial class ApiConnection
 	}
 #endregion
 
-	///////////////////////////////////////////////////
+
 	///////////////////////////////////////////////////
 
 
 #region ParseMessage
-	async Task ParseMessage(MessageCode hycybh, string message)
+	async Task ParseMessage(MessageCode messageCode, string message)
 	{
 		JObject json;
 		try
 		{
-			json = ParseToJObject(message, hycybh);
+			json = ParseToJObject(message, messageCode);
 		}
 		catch(Exception)
 		{
@@ -113,379 +113,379 @@ public partial class ApiConnection
 		}
 
 		List<Task> tasks = [];
-		switch (hycybh)
+		switch (messageCode)
 		{
 #region STA
 			case MessageCode.STA:
-				{
-					tasks.Add(Task.Run(() =>
+			{
+				tasks.Add(Task.Run(() =>
+					{
+						if (json["character"].ToString().Equals(CharacterName, StringComparison.InvariantCultureIgnoreCase))
 						{
-							if (json["character"].ToString().Equals(CharacterName, StringComparison.InvariantCultureIgnoreCase))
-							{
-								Console.WriteLine("Status Changed");
-							}
+							Console.WriteLine("Status Changed");
 						}
-					));
-				}
-				break;
+					}
+				));
+			}
+			break;
 #endregion
 
 #region IDN
 			case MessageCode.IDN:
-				{
-					ConnectedToChat?.Invoke(this, null);
+			{
+				ConnectedToChat?.Invoke(this, null);
 
-					tasks.Add(RequestInternalChannelList(ChannelType.Private));
-					tasks.Add(RequestInternalChannelList(ChannelType.Public));
+				tasks.Add(RequestInternalChannelList(ChannelType.Private));
+				tasks.Add(RequestInternalChannelList(ChannelType.Public));
 
-					Console.WriteLine("Connected to Chat");
-				}
-				break;
+				Console.WriteLine("Connected to Chat");
+			}
+			break;
 #endregion
 
 #region ORS
 			case MessageCode.ORS:
+			{
+				List<Channel> privateChannelList = [];
+				foreach (var channel in json["channels"])
 				{
-					List<Channel> privateChannelList = [];
-					foreach (var channel in json["channels"])
-					{
-						privateChannelList.Add(new Channel(channel["title"].ToString(), channel["name"].ToString(), ChannelType.Private));
-					}
-
-					tasks.Add(Task.Run(() =>
-						{
-							ChannelTracker.RefreshAvailableChannels(privateChannelList, ChannelType.Private);
-							PrivateChannelsReceivedHandler?.Invoke(this, new ChannelEventArgs() { });
-							Console.WriteLine($"Private Channels Recieved... {privateChannelList.Count} total Private Channels.");
-						}
-					));
+					privateChannelList.Add(new Channel(channel["title"].ToString(), channel["name"].ToString(), ChannelType.Private));
 				}
-				break;
+
+				tasks.Add(Task.Run(() =>
+					{
+						ChannelTracker.RefreshAvailableChannels(privateChannelList, ChannelType.Private);
+						PrivateChannelsReceivedHandler?.Invoke(this, new ChannelEventArgs() { });
+						Console.WriteLine($"Private Channels Recieved... {privateChannelList.Count} total Private Channels.");
+					}
+				));
+			}
+			break;
 #endregion
 
 #region CHA
 			case MessageCode.CHA:
+			{
+				List<Channel> publicChannelList = [];
+				foreach (var channel in json["channels"])
 				{
-					List<Channel> publicChannelList = [];
-					foreach (var channel in json["channels"])
-					{
-						string name = channel["name"].ToString();
-						publicChannelList.Add(new Channel(name, name, ChannelType.Public));
-					}
-
-					tasks.Add(Task.Run(() =>
-						{
-							ChannelTracker.RefreshAvailableChannels(publicChannelList, ChannelType.Public);
-							PublicChannelsReceivedHandler?.Invoke(this, new ChannelEventArgs() { });
-							Console.WriteLine($"Public Channels Recieved... {publicChannelList.Count} total Public Channels.");
-						}
-					));
+					string name = channel["name"].ToString();
+					publicChannelList.Add(new Channel(name, name, ChannelType.Public));
 				}
-				break;
+
+				tasks.Add(Task.Run(() =>
+					{
+						ChannelTracker.RefreshAvailableChannels(publicChannelList, ChannelType.Public);
+						PublicChannelsReceivedHandler?.Invoke(this, new ChannelEventArgs() { });
+						Console.WriteLine($"Public Channels Recieved... {publicChannelList.Count} total Public Channels.");
+					}
+				));
+			}
+			break;
 #endregion
 
 #region MSG
 			case MessageCode.MSG:
-				{
-					MessageHandler?.Invoke(MessageCode.MSG, new MessageEventArgs() { channel = json["channel"].ToString(), message = json["message"].ToString(), user = json["character"].ToString() });
-					Console.WriteLine(message);
-				}
-				break;
+			{
+				MessageHandler?.Invoke(MessageCode.MSG, new MessageEventArgs() { channel = json["channel"].ToString(), message = json["message"].ToString(), user = json["character"].ToString() });
+				Console.WriteLine(message);
+			}
+			break;
 #endregion
 
 #region PRI
 			case MessageCode.PRI:
-				{
-					MessageHandler?.Invoke(MessageCode.PRI, new MessageEventArgs() { user = json["character"].ToString(), message = json["message"].ToString() });
-					Console.WriteLine(message);
-				}
-				break;
+			{
+				MessageHandler?.Invoke(MessageCode.PRI, new MessageEventArgs() { user = json["character"].ToString(), message = json["message"].ToString() });
+				Console.WriteLine(message);
+			}
+			break;
 #endregion
 
 #region JCH
 			case MessageCode.JCH:
+			{
+				User user = UserTracker.GetUserByName(json["character"]["identity"].ToString());
+				Channel channel;
+				try
 				{
-					User user = UserTracker.GetUserByName(json["character"]["identity"].ToString());
-					Channel channel;
-					try
-					{
-						channel = ChannelTracker.GetChannelByNameOrCode(json["title"].ToString());
-					}
-					catch
-					{
-						channel = ChannelTracker.AddManualChannel(json["title"].ToString(), ChannelStatus.Available, json["channel"].ToString());
-					}
+					channel = ChannelTracker.GetChannelByNameOrCode(json["title"].ToString());
+				}
+				catch
+				{
+					channel = ChannelTracker.AddManualChannel(json["title"].ToString(), ChannelStatus.Available, json["channel"].ToString());
+				}
 
-					if (user.Name.Equals(CharacterName))
-					{
-						ChannelTracker.WatchChannels.Add(channel.Code,channel);
-					}
+				if (user.Name.Equals(CharacterName))
+				{
+					ChannelTracker.WatchChannels.Add(channel.Code,channel);
+				}
 
-					if (channel == null)
-					{
-						return;
-					}
+				if (channel == null)
+				{
+					return;
+				}
 
-					// creating channel
-					bool creating = channel.Status == ChannelStatus.Creating && user.Name.Equals(CharacterName);
-					if (creating)
+				// creating channel
+				bool creating = channel.Status == ChannelStatus.Creating && user.Name.Equals(CharacterName);
+				if (creating)
+				{
+					channel = ChannelTracker.FinalizeChannelCreation(json["title"].ToString(), json["channel"].ToString(), user);
+					Console.WriteLine($"Created Channel: {json["channel"]}");
+					CreatedChannelHandler?.Invoke(this, new ChannelEventArgs() { name = channel.Name, status = ChannelStatus.Joined, code = channel.Code, type = channel.Type });
+				}
+				else
+				{
+					// join channel
+					channel = ChannelTracker.GetChannelByNameOrCode(json["channel"].ToString());
+					if (user != null && channel != null)
 					{
-						channel = ChannelTracker.FinalizeChannelCreation(json["title"].ToString(), json["channel"].ToString(), user);
-						Console.WriteLine($"Created Channel: {json["channel"]}");
-						CreatedChannelHandler?.Invoke(this, new ChannelEventArgs() { name = channel.Name, status = ChannelStatus.Joined, code = channel.Code, type = channel.Type });
-					}
-					else
-					{
-						// join channel
-						channel = ChannelTracker.GetChannelByNameOrCode(json["channel"].ToString());
-						if (user != null && channel != null)
+						if (!creating)
 						{
-							if (!creating)
-							{
-								JoinedChannelHandler?.Invoke(this, new ChannelEventArgs() { name = json["title"].ToString(), status = ChannelStatus.Joined, code = channel.Code, type = channel.Type, userJoining = user.Name });
-							}
-
-							tasks.Add(Task.Run(() =>
-								{
-									channel.AddUser(user);
-									Console.WriteLine($"{user.Name} joined Channel: {channel.Name}. {channel.Users.Count} total users in channel.");
-								}
-							));
+							JoinedChannelHandler?.Invoke(this, new ChannelEventArgs() { name = json["title"].ToString(), status = ChannelStatus.Joined, code = channel.Code, type = channel.Type, userJoining = user.Name });
 						}
+
+						tasks.Add(Task.Run(() =>
+							{
+								channel.AddUser(user);
+								Console.WriteLine($"{user.Name} joined Channel: {channel.Name}. {channel.Users.Count} total users in channel.");
+							}
+						));
 					}
 				}
-				break;
+			}
+			break;
 #endregion
 
 #region LCH
 			case MessageCode.LCH:
+			{
+				if (json["character"].ToString().Equals(CharacterName, StringComparison.InvariantCultureIgnoreCase))
 				{
-					if (json["character"].ToString().Equals(CharacterName, StringComparison.InvariantCultureIgnoreCase))
-					{
-						LeftChannelHandler?.Invoke(this, new ChannelEventArgs() { name = json["channel"].ToString(), status = ChannelStatus.Left });
-						ChannelTracker.ChangeChannelStatus(json["channel"].ToString(), ChannelStatus.Available);
-					}
-
-					User user = UserTracker.GetUserByName(json["character"].ToString());
-					Channel channel = ChannelTracker.GetChannelByNameOrCode(json["channel"].ToString());
-
-					if (user.Name.Equals(CharacterName))
-					{
-						ChannelTracker.WatchChannels.Remove(channel.Code);
-					}
-
-					if (user != null && channel != null)
-					{
-
-						tasks.Add(Task.Run(() =>
-							{
-								channel.RemoveUser(user);
-								Console.WriteLine($"{user.Name} left Channel: {json["channel"]}. {channel.Users.Count} total users in channel.");
-							}
-						));
-					}
-
+					LeftChannelHandler?.Invoke(this, new ChannelEventArgs() { name = json["channel"].ToString(), status = ChannelStatus.Left });
+					ChannelTracker.ChangeChannelStatus(json["channel"].ToString(), ChannelStatus.Available);
 				}
-				break;
+
+				User user = UserTracker.GetUserByName(json["character"].ToString());
+				Channel channel = ChannelTracker.GetChannelByNameOrCode(json["channel"].ToString());
+
+				if (user.Name.Equals(CharacterName))
+				{
+					ChannelTracker.WatchChannels.Remove(channel.Code);
+				}
+
+				if (user != null && channel != null)
+				{
+
+					tasks.Add(Task.Run(() =>
+						{
+							channel.RemoveUser(user);
+							Console.WriteLine($"{user.Name} left Channel: {json["channel"]}. {channel.Users.Count} total users in channel.");
+						}
+					));
+				}
+
+			}
+			break;
 #endregion
 
 #region PIN
 			case MessageCode.PIN:
-				{
-					tasks.Add(Client.SendAsync(MessageCode.PIN.ToString()));
-				}
-				break;
+			{
+				tasks.Add(Client.SendAsync(MessageCode.PIN.ToString()));
+			}
+			break;
 #endregion
 
 #region VAR
 			case MessageCode.VAR:
-				{
+			{
 
-				}
-				break;
+			}
+			break;
 #endregion
 
 #region HLO
 			case MessageCode.HLO:
-				{
+			{
 
-				}
-				break;
+			}
+			break;
 #endregion
 
 #region CON
 			case MessageCode.CON:
-				{
-					tasks.Add(Task.Run(() => Console.WriteLine($"{json["count"]} connected users sent.")));
-				}
-				break;
+			{
+				tasks.Add(Task.Run(() => Console.WriteLine($"{json["count"]} connected users sent.")));
+			}
+			break;
 #endregion
 
 #region FRL
 			case MessageCode.FRL:
-				{
-					// friends list
-				}
-				break;
+			{
+				// friends list
+			}
+			break;
 #endregion
 
 #region IGN
 			case MessageCode.IGN:
-				{
-					// ignore list
-				}
-				break;
+			{
+				// ignore list
+			}
+			break;
 #endregion
 
 #region ADL
 			case MessageCode.ADL:
-				{
+			{
 
-				}
-				break;
+			}
+			break;
 #endregion
 
 #region LIS
 			case MessageCode.LIS:
+			{
+				foreach(var userinfo in json["characters"])
 				{
-					foreach(var userinfo in json["characters"])
+					User tempUser = new()
 					{
-						User tempUser = new()
-						{
-							Name		= userinfo[0].ToString(),
-							Gender		= userinfo[1].ToString(),
-							ChatStatus	= (ChatStatus)Enum.Parse(typeof(ChatStatus), userinfo[2].ToString().ToLowerInvariant(), true)
-						};
-						if (!UserTracker.TryAddUser(tempUser))
-						{
-						tasks.Add(Task.Run(
-								() => UserTracker
-									.GetUserByName(tempUser.Name)
-									.Update(tempUser)));
-						}
-						//UserTracker.SetChatStatus(tempUser, tempUser.ChatStatus, false);
+						Name		= userinfo[0].ToString(),
+						Gender		= userinfo[1].ToString(),
+						ChatStatus	= (ChatStatus)Enum.Parse(typeof(ChatStatus), userinfo[2].ToString().ToLowerInvariant(), true)
+					};
+					if (!UserTracker.TryAddUser(tempUser))
+					{
+					tasks.Add(Task.Run(
+							() => UserTracker
+								.GetUserByName(tempUser.Name)
+								.Update(tempUser)));
 					}
-#if DEBUG
-					tasks.Add(Task.Run(() => Console.WriteLine($"Added {json["characters"].Count()} users. Total users: {UserTracker.GetNumberActiveUsers()}")));
-#endif
+					//UserTracker.SetChatStatus(tempUser, tempUser.ChatStatus, false);
 				}
-				break;
+#if DEBUG
+				tasks.Add(Task.Run(() => Console.WriteLine($"Added {json["characters"].Count()} users. Total users: {UserTracker.GetNumberActiveUsers()}")));
+#endif
+			}
+			break;
 #endregion
 
 #region NLN
 			case MessageCode.NLN:
+			{
+				User tempUser = new()
 				{
-					User tempUser = new()
-					{
-						Name = json["identity"].ToString(),
-						UserStatus = (UserStatus)Enum.Parse(typeof(UserStatus), json["status"].ToString().ToLowerInvariant(), true),
-						Gender = json["gender"].ToString()
-					};
+					Name = json["identity"].ToString(),
+					UserStatus = (UserStatus)Enum.Parse(typeof(UserStatus), json["status"].ToString().ToLowerInvariant(), true),
+					Gender = json["gender"].ToString()
+				};
 
-					tasks.Add(Task.Run(() => UserTracker.SetChatStatus(tempUser, ChatStatus.Online, false)));
-				}
-				break;
+				tasks.Add(Task.Run(() => UserTracker.SetChatStatus(tempUser, ChatStatus.Online, false)));
+			}
+			break;
 #endregion
 
 #region COL
 			case MessageCode.COL:
+			{
+				Channel channel = ChannelTracker.GetChannelByNameOrCode(json["channel"].ToString());
+
+				bool ownerAdded = false;
+				foreach (string username in json["oplist"].Select(user => user.ToString()))
 				{
-					Channel channel = ChannelTracker.GetChannelByNameOrCode(json["channel"].ToString());
-
-					bool ownerAdded = false;
-					foreach (string username in json["oplist"].Select(user => user.ToString()))
+					if (string.IsNullOrWhiteSpace(username))
 					{
-						if (string.IsNullOrWhiteSpace(username))
-						{
-							continue;
-						}
-
-						User user = UserTracker.GetUserByName(username);
-
-						if (!ownerAdded)
-						{
-							channel.Owner	= user;
-							ownerAdded		= true;
-						}
-
-						tasks.Add(Task.Run(() => channel.AddMod(user)));
+						continue;
 					}
-#if DEBUG
-					tasks.Add(Task.Run(() => Console.WriteLine($"Found {channel.Mods.Count} mods for channel: {channel.Name}")));
-#endif
+
+					User user = UserTracker.GetUserByName(username);
+
+					if (!ownerAdded)
+					{
+						channel.Owner	= user;
+						ownerAdded		= true;
+					}
+
+					tasks.Add(Task.Run(() => channel.AddMod(user)));
 				}
-				break;
+#if DEBUG
+				tasks.Add(Task.Run(() => Console.WriteLine($"Found {channel.Mods.Count} mods for channel: {channel.Name}")));
+#endif
+			}
+			break;
 #endregion
 
 #region FLN
 			case MessageCode.FLN:
-				{
-					User user = UserTracker.GetUserByName(json["character"].ToString());
-					UserTracker.SetChatStatus(user, ChatStatus.Offline, false);
+			{
+				User user = UserTracker.GetUserByName(json["character"].ToString());
+				UserTracker.SetChatStatus(user, ChatStatus.Offline, false);
 
-					tasks.Add(Task.Run(() =>
+				tasks.Add(Task.Run(() =>
+					{
+						foreach (var channel in ChannelTracker.WatchChannels.Values)
 						{
-							foreach (var channel in ChannelTracker.WatchChannels.Values)
+							bool needsRemoved = false;
+
+							if (channel.Users.ContainsKey(user.Name))
 							{
-								bool needsRemoved = false;
+								needsRemoved = true;
+							}
 
-								if (channel.Users.ContainsKey(user.Name))
-								{
-									needsRemoved = true;
-								}
-
-								if (needsRemoved)
-								{
-									channel.RemoveUser(user);
-								}
+							if (needsRemoved)
+							{
+								channel.RemoveUser(user);
 							}
 						}
-					));
-				}
-				break;
+					}
+				));
+			}
+			break;
 #endregion
 
 #region ICH
 			case MessageCode.ICH:
+			{
+				// joining channel
+				Channel channel = ChannelTracker.ChangeChannelStatus(json["channel"].ToString(), ChannelStatus.Joined);
+
+				foreach (User user in json["users"].Select(u => UserTracker.GetUserByName(u["identity"].ToString())))
 				{
-					// joining channel
-					Channel channel = ChannelTracker.ChangeChannelStatus(json["channel"].ToString(), ChannelStatus.Joined);
-
-					foreach (User user in json["users"].Select(u => UserTracker.GetUserByName(u["identity"].ToString())))
-					{
-						tasks.Add(Task.Run(() =>
+					tasks.Add(Task.Run(() =>
+						{
+							if (null == user)
 							{
-								if (null == user)
-								{
-									Console.WriteLine($"Error attempting to add user {user.Name} to {channel.Name} channel's userlist.");
-								}
-
-								channel.AddUser(user);
-								channel.AdEnabled = !json["mode"].ToString().Equals("chat");
+								Console.WriteLine($"Error attempting to add user {user.Name} to {channel.Name} channel's userlist.");
 							}
-						));
-					}
-#if DEBUG
-					tasks.Add(Task.Run(() => Console.WriteLine($"Adding {json["users"].Count()} users to {channel.Name} channel's userlist successful.")));
-#endif
+
+							channel.AddUser(user);
+							channel.AdEnabled = !json["mode"].ToString().Equals("chat");
+						}
+					));
 				}
-				break;
+#if DEBUG
+				tasks.Add(Task.Run(() => Console.WriteLine($"Adding {json["users"].Count()} users to {channel.Name} channel's userlist successful.")));
+#endif
+			}
+			break;
 #endregion
 
 #region CDS
 			case MessageCode.CDS:
-				{
-					tasks.Add(Task.Run(() => ChannelTracker.GetChannelByNameOrCode(json["channel"].ToString()).Description = json["description"].ToString()));
-				}
-				break;
+			{
+				tasks.Add(Task.Run(() => ChannelTracker.GetChannelByNameOrCode(json["channel"].ToString()).Description = json["description"].ToString()));
+			}
+			break;
 #endregion
 
 #region default
 			default:
-				{
+			{
 
-				}
-				break;
+			}
+			break;
 #endregion
 		}
 		await Task.WhenAll([.. tasks]);
